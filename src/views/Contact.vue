ee26d6d126704558b3963c5c726fa318e1c74a0f
araquach <adam@contra-design.com>
2020-10-22 14:47:47 +0100

    Fixed link from salon to contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -48,7 +48,6 @@
         </figure>
       </section>
     </div>
-
   </div>
 
   </div>
--- src/views/Salon.vue
+++ src/views/Salon.vue
@@ -12,7 +12,7 @@
           <p class="is-size-5">With a prime position right on the corner of <em>Sankey Street</em> and <em>Springfield street</em> the salon premises is an early 1900's building that was once home to the Picturedrome and Cameo cinemas! It's taken on many guises since, but when we got hold of it we knew we wanted it to be the warmest, friendliest most relaxing salon in the area.</p>
           <p>The salon was designed to create the ultimate in relaxing environments. With cosy, defined areas, you never feel too exposed, but its still open enough to soak up the vibrant, sociable atmosphere. The warm, friendly staff are always on hand to make you feel at home. Enjoy a cappuccino, latte or a glass of wine whilst you are being pampered.</p>
           <p>If you're traveling to us by car, there are loads of car park facilities nearby. By train or bus, its only a short walk from both train stations and the bus terminal. </p>
-          <router-link :to="{ name: 'salon' }">Map and more info here</router-link>
+          <router-link :to="{ name: 'contact' }">Map and more info here</router-link>
         </div>
       </div>
       <div class="tile is-parent is-vertical">
