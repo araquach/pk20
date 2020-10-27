39b751e124bdc095dcd2fafda20e0adea20dc604
Adam Carter <adam@contra-design.com>
2020-10-27 08:28:50 +0000

    Minor style chanages

--- src/views/Reviews.vue
+++ src/views/Reviews.vue
@@ -1,37 +1,39 @@
 <template>
-  <div id="reviews" class="section columns">
-    <div class="column is-8">
-      <h2 class="title is-3 has-text-primary">Some of our recent client reviews</h2>
-      <h3 class="subtitle is-5 has-text-primary">Filter by stylist</h3>
-      <div class="columns is-multiline is-mobile">
-        <div v-for="tm in teamMembers" class="column is-2-desktop is-2-tablet is-4-mobile">
-          <router-link :to="{ name: 'reviews', params: { slug: tm.slug } }">
-            <div class="card">
-              <div class="card-image">
-                <figure class="image is-small">
-                  <img :src="tm.image">
-                </figure>
-              </div>
-              <div class="card content has-background-primary">
-                <div class="content has-text-white">
-                  {{ tm.first_name }}
+  <div id="reviews">
+    <div class="section columns">
+      <div class="column is-8">
+        <h2 class="title is-3 has-text-primary">Some of our recent client reviews</h2>
+        <h3 class="subtitle is-5 has-text-primary">Filter by stylist</h3>
+        <div class="columns is-multiline is-mobile">
+          <div v-for="tm in teamMembers" class="column is-2-desktop is-2-tablet is-4-mobile">
+            <router-link :to="{ name: 'reviews', params: { slug: tm.slug } }">
+              <div class="card">
+                <div class="card-image">
+                  <figure class="image is-small">
+                    <img :src="tm.image">
+                  </figure>
+                </div>
+                <div class="card content has-background-primary">
+                  <div class="content has-text-white">
+                    {{ tm.first_name }}
+                  </div>
                 </div>
               </div>
-            </div>
-          </router-link>
+            </router-link>
+          </div>
+        </div>
+
+        <h2 class="title is-4" >{{ stylist }} Reviews</h2>
+
+        <div v-for="review in reviews" class="box">
+          <p class="is-size-5">"{{ review.review }}"</p>
+          <small>{{ review.client }}</small><br>
+          <small>Hair by: {{ review.stylist }}</small>
         </div>
       </div>
-
-      <h2 class="title is-4" >{{ stylist }} Reviews</h2>
-
-      <div v-for="review in reviews" class="box">
-        <p class="is-size-5">"{{ review.review }}"</p>
-        <small>{{ review.client }}</small><br>
-        <small>Hair by: {{ review.stylist }}</small>
+      <div class="column">
+        <img src="/dist/img/reviews/reviews-background.jpg">
       </div>
-    </div>
-    <div class="column">
-      <img src="/dist/img/reviews/reviews-background.jpg">
     </div>
   </div>
 </template>
