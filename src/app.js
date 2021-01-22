a3a37880813118a009eaf52a7bd30c68e234d811
Adam Carter <adam@contra-design.com>
2021-01-22 16:52:10 +0000

    Updated the router

--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import Vuelidate from 'vuelidate'
 import App from './App.vue'
-import { routes } from "./routes"
+import router from "./router"
 import { store } from './store/store'
 import VueMoment from "vue-moment"
 
@@ -10,28 +9,12 @@ import Buefy from 'buefy'
 
 Vue.use(Buefy)
 Vue.use(Vuelidate)
-Vue.use(VueRouter)
 Vue.use(VueMoment)
 
 window.axios = require('axios')
 
 Vue.filter('textLimit', function (text, length) {
     return text.substring(0, length)
-})
-
-const router = new VueRouter({
-    mode: 'history',
-    scrollBehavior (to, from, savedPosition) {
-        if (to.hash) {
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve({ selector: to.hash, offset: { x: 0, y: 0 }})
-                }, 500)
-            })
-        }
-        return { x: 0, y: 0 }
-    },
-    routes
 })
 
 new Vue({
