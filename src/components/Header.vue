7b9014a874bc784202a44bc3b778be3c56b418b7
araquach <adam@contra-design.com>
2021-08-31 13:53:54 +0100

    Updated the Meta

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,9 +8,9 @@
           </b-navbar-item>
         </template>
         <template slot="start">
-          <b-navbar-item tag="router-link" :to="{ name: 'home' }">
-            Home
-          </b-navbar-item>
+<!--          <b-navbar-item tag="router-link" :to="{ name: 'home' }">-->
+<!--            Home-->
+<!--          </b-navbar-item>-->
           <b-navbar-item tag="router-link" :to="{ name: 'salon' }">
             Salon
           </b-navbar-item>
@@ -20,9 +20,9 @@
           <b-navbar-item tag="router-link" :to="{ name: 'prices' }">
             Prices
           </b-navbar-item>
-<!--          <b-navbar-item tag="router-link" :to="{ name: 'offers' }">-->
-<!--            Offers-->
-<!--          </b-navbar-item>-->
+          <b-navbar-item tag="router-link" :to="{ name: 'offers' }">
+            Offers
+          </b-navbar-item>
 
           <b-navbar-dropdown label="More">
 <!--            <b-navbar-item tag="router-link" :to="{ name: 'corona' }">-->
