a6bda6e8be4921f99433fd287a693084fab51234
Adam Carter <adam@contra-design.com>
2021-09-09 11:37:48 +0100

    Updated Price Calc again

--- src/views/TeamDetail.vue
+++ src/views/TeamDetail.vue
@@ -7,6 +7,8 @@
         </figure>
         <div id="feedback_feed_ind" class="box is-shadowless">
           <ReviewFeed/>
+          <br>
+          <router-link class="is-primary" :to="{ name: 'reviews', params: 'slug' }">More of {{ teamMember.first_name }}'s reviews here ></router-link>
         </div>
       </div>
       <div class="column section is-size-6 is-size-5-mobile">
@@ -17,13 +19,10 @@
         <p>{{ teamMember.para_3 }}</p>
         <p class="has-text-weight-bold is-size-5 is-bold">Average Cut &amp; Colour price with {{ teamMember.first_name }}: &pound;{{ teamMember.price }}*</p>
         <div class="team-ind-bottom">
-          <router-link class="button is-primary" :to="{ name: 'reviews', params: 'slug' }">More of {{ teamMember.first_name }}'s reviews here</router-link>
+          <router-link class="button is-primary" :to="{ name: 'stylist-prices', params: { stylist: teamMember.slug} }">Get a quote with {{ teamMember.first_name }}</router-link>
           <br><br>
           <router-link class="button is-primary" :to="{ name: 'team', hash: '#' + teamMember.slug }">Back to the full team</router-link>
           <br><br>
-          <div id="fb-like">
-            <div class="fb-like" :data-href="fbLink" data-width="250" data-layout="button" data-action="like" data-show-faces="true" data-share="true"></div>
-          </div>
           <section>
             <p class="has-text-centered is-size-7">*average prices for half-head foils or equivalent plus a Cut Dry and Style - before any discounts</p>
           </section>
