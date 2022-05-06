ef785a12a69ed829786379a4417d441ffbaf9b1f
araquach <adam@contra-design.com>
2022-05-06 10:41:48 +0100

    Fixed typo

--- src/views/TopSpender.vue
+++ src/views/TopSpender.vue
@@ -4,7 +4,7 @@
       <div class="column is-7-desktop">
         <h1 class="title is-2">A BIG thank you from the team</h1>
         <p class="is-size-4">As a loyal customer to the salon we'd like to thank you for your continued support.</p>
-        <p>We've had a crazy few years and we're really thank you for your patience and adaptability through the tough times.</p>
+        <p>We've had a crazy few years and we're really thankful for your patience and adaptability through the tough times.</p>
         <p class="is-size-4">PK wouldn't be here without you, and for that we'd like to give you a little thank you gift....</p>
         <p>This Schwarzkopf Gift Set - choose from Colour Freeze or Repair Rescue</p>
         <figure class="image">
