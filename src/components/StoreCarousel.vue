7f99cc6e84895ad35ab08e30420d2dfc9bb3d245
araquach <adam@contra-design.com>
2023-10-27 09:32:19 +0100

    Added click region

--- src/components/StoreCarousel.vue
+++ src/components/StoreCarousel.vue
@@ -1,13 +1,22 @@
 <template>
   <b-carousel :indicator-inside="true">
     <b-carousel-item v-for="(item, i) in banners" :key="i">
-      <a :href="item.url">
-        <b-image class="image is-hidden-tablet" :src="`/dist/img/store/banners/mobile/${item.image}`"></b-image>
-        <b-image class="image is-hidden-mobile" :src="`/dist/img/store/banners/desktop/${item.image}`"></b-image>
-      </a>
+      <div class="image-container">
+        <!-- Mobile image -->
+        <b-image class="image is-hidden-tablet-only" :src="`/dist/img/store/banners/mobile/${item.image}`"></b-image>
+
+        <!-- Clickable Area for Mobile: Adjust positioning as needed -->
+        <a :href="item.url" class="clickable-region is-hidden-tablet-only"></a>
+
+        <!-- Desktop/Tablet image -->
+        <a :href="item.url">
+          <b-image class="image is-hidden-mobile" :src="`/dist/img/store/banners/desktop/${item.image}`"></b-image>
+        </a>
+      </div>
     </b-carousel-item>
   </b-carousel>
 </template>
+
 
 <script>
 export default {
@@ -22,4 +31,27 @@ export default {
 .al img {
   filter: grayscale(100%);
 }
+
+.image-container {
+  position: relative;
+}
+
+.clickable-region {
+  position: absolute;
+  top: 5%;  /* Adjust these values to position */
+  right: 15%; /* the clickable region correctly on mobile */
+  width: 200px;  /* Set the size of the clickable area */
+  height: 120px; /* Set the size of the clickable area */
+  cursor: pointer;
+  z-index: 1; /* To ensure it's above the image */
+}
+
+.is-active .al img {
+  filter: grayscale(0%);
+}
+
+.al img {
+  filter: grayscale(100%);
+}
+
 </style>
