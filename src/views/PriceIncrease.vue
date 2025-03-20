2a13ef746007c288c1fb015037f4a558e5a27011
araquach <adam@contra-design.com>
2025-03-20 15:06:29 +0000

    changed header

--- src/views/PriceIncrease.vue
+++ src/views/PriceIncrease.vue
@@ -3,7 +3,7 @@
     <div class="columns">
       <div class="column is-8">
         <section class="section is-size-5">
-          <h1 class="title is-2 has-text-white">Just to let you know...</h1>
+          <h1 class="title is-2">Just to let you know...</h1>
           <p class="is-size-4">From your next appointment, there will be a small price adjustment. This is due to recent increases in the minimum wage, employer National Insurance contributions, and the rising cost of stock and energy prices.</p>
           <p>We want to continue offering the same amazing service you’re used to. We’re also committed to training and developing the next generation of talented stylists, ensuring our high standards continue long into the future.</p>
           <p>We truly value our loyal clients and want to be as fair as possible while ensuring the long-term health of the business. It’s always a fine balance, and we’re so grateful for your understanding.</p>
