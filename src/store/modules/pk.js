af4645be0464bbdf3a69a580957f60cfa23903f1
araquach <adam@contra-design.com>
2025-09-02 12:00:04 +0100

    Turned Off Offers

--- src/store/modules/pk.js
+++ src/store/modules/pk.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const today = new Date()
 
 export const state = {
-    hideOffers: false,
+    hideOffers: true,
     endDate: "30/06/25",
     newsItems: [],
     storeData: {}
