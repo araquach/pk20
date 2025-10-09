eabc90fd96bf17ec5773bb0fa310b0452bd702da
araquach <adam@contra-design.com>
2025-10-09 16:39:27 +0100

    Changed styling

--- src/views/offers/Oct25.vue
+++ src/views/offers/Oct25.vue
@@ -3,8 +3,8 @@
     <div class="columns">
       <div class="column is-8">
         <div class="top section">
-          <h2 class="title is-2 is-size-4-mobile has-text-primary mb-3">Treat yourself to a new look!</h2>
-          <h2 class="title is-4 is-size-6-mobile has-text-primary">Take advantage of our exclusive new client offer</h2>
+          <h2 class="title is-2 has-text-primary mb-3">Treat yourself to a new look!</h2>
+          <h2 class="title is-4 has-text-primary">Take advantage of our exclusive new client offer</h2>
           <h2 class="title-new-mid is-2 mb-0"><strong>25% OFF your first visit!</strong></h2>
         </div>
       </div>
@@ -18,10 +18,10 @@
           <p class="is-size-5"><strong>Book in for a FREE consultation with one of our talented stylists to take advantage of our new client offer.  We have stylists for a variety of budgets. </strong></p>
           <h3 class="title is-3 has-text-primary">Average Colour & Cut price for...</h3>
           <ul class="is-size-5">
-            <li class="mb-3">Advanced Senior Stylist: <strong>£170</strong> (£130 inc discount)</li>
-            <li class="mb-3">Senior Stylist: <strong>£160</strong> (£120 inc discount))</li>
-            <li class="mb-3">Stylist: <strong>£130</strong> (£100 inc discount)</li>
-            <li>Graduate Stylist: <strong>£100</strong> (£75 inc discount)</li>
+            <li class="mb-3"><strong>Advanced Senior Stylist: £170 (£130 inc discount)</strong></li>
+            <li class="mb-3"><strong>Senior Stylist: £160 (£120 inc discount))</strong></li>
+            <li class="mb-3"><strong>Stylist: £130 (£100 inc discount)</strong></li>
+            <li><strong>Graduate Stylist: £100 (£75 inc discount)</strong></li>
           </ul>
           <br>
           <router-link class="button is-primary" :to="{ name: 'team' }">Browse Stylists</router-link>
