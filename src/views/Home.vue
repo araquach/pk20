72eba739d44d15fe832ec338527497b48797fa43
araquach <adam@contra-design.com>
2026-03-12 12:45:51 +0000

    Added Sloane Reference

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,10 +7,15 @@
     <div class="columns is-marginless">
       <div class="column is-5 is-offset-7">
         <section class="section is-size-5-mobile">
-          <h2 class="title 2 has-text-primary has-text-centered">A New Standard of Hairdressing</h2>
-          <p>Paul Kemp Hairdressing is located right in the heart of Warrington town centre. Sister salon to the award winning <em><a href="http://www.jakatasalon.co.uk/" target="_blank">Jakata</a></em>, the stunning salon opened in June 2011 with the aim to offer an ultra relaxing atmosphere, first class customer service, alongside the highest level of hairdressing expertise.</p>
-          <p>The talented hairdressing team are all trained to the highest level in cutting, colouring and styling hair, with specialists in technical colour, hair straightening, wedding hair and hair extensions. The team has a wealth of experience in all aspects of hairdressing so you can just sit back, relax and enjoy the experience.</p>
-          <p><em>The salon has built up a strong reputation for creating stunning, fashionable looks tailored to suit you, and your lifestyle, so if you're looking for a new <strong class="paraend">hairdressers in Warrington</strong> then look no further!</em></p>
+          <h2 class="title 2 has-text-primary has-text-centered">A New Chapter</h2>
+          <p>After many years as Paul Kemp Hairdressing, our Sankey Street salon is entering an exciting new chapter.</p>
+          <p>Following a complete refurbishment of the space, we’re introducing</p>
+          <div class="has-text-centered mb-3">
+            <img src="/dist/img/logos/sloane_logo.png" class="sloane-logo" alt="Sloane The Salon">
+          </div>
+          <p>A refined evolution of the salon you’ve always known.</p>
+          <p>While the name and environment are changing, the heart of what we do remains exactly the same: exceptional hair, thoughtful service, and the talented team you trust.</p>
+          <p><em>Over the coming months you’ll see the new identity gradually introduced as we transition into this next phase. For now, simply enjoy the refreshed space and the same great experience you’ve always had with us.</em></p>
           <section id="feedback-feed-container">
             <div id="feedback_feed" class="box">
               <ReviewFeed :staffMember="staffMember"/>
@@ -68,3 +73,9 @@
 
   }
 </script>
+<style>
+.sloane-logo {
+  width: 70%;
+
+}
+</style>
