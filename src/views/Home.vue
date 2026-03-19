806369694e66b9a6650117f12bd0607586559c21
araquach <adam@contra-design.com>
2026-03-19 08:24:40 +0000

    Added Sloane Link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,6 +16,9 @@
           <p>A refined evolution of the salon you’ve always known.</p>
           <p>While the name and environment are changing, the heart of what we do remains exactly the same: exceptional hair, thoughtful service, and the talented team you trust.</p>
           <p><em>Over the coming months you’ll see the new identity gradually introduced as we transition into this next phase. For now, simply enjoy the refreshed space and the same great experience you’ve always had with us.</em></p>
+          <div class="buttons is-right">
+            <router-link :to="{ name: 'rebrand'}" class="button is-primary">Find out more</router-link>
+          </div>
           <section id="feedback-feed-container">
             <div id="feedback_feed" class="box">
               <ReviewFeed :staffMember="staffMember"/>
